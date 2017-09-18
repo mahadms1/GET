f89dcdadee37ed5fbc06885935f35df0f1da5871
mahadms1 <mahalakshmi.dms1@gmail.com>
2017-09-18 17:55:50 +0530

    flie are commited

---
+++ src/main/java/com/pmc/App.java
@@ -0,0 +1,13 @@
+package com.pmc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
